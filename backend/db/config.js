166632edf6d58ef9b34be9463db2683d8dedadbb
Akshat103 <akshattamrakar103@gmail.com>
2023-04-04 21:03:18 +0530

    Starting Code Cleaning

---
+++ backend/db/config.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://akshattamrakar:root@cluster0.0z0aan1.mongodb.net/tambola?retryWrites=true&w=majority');
